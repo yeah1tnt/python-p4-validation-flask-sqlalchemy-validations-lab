490c0a52f97e565eae16d47a805b1ddd4d2e36b5
Tong Tang <pichip22@hotmail.com>
2023-09-23 08:51:55 -0400

    change squlite path

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from models import db, Author, Post
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
